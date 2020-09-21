6c04abdcd164774720bc0d5560f347cc06429fe9
Your Name <you@example.com>
2020-09-21 18:35:04 -0300

    Updates

--- src/_modules/slider/slider.js
+++ src/_modules/slider/slider.js
@@ -16,7 +16,7 @@ var Slider = function() {
                 fade: true,
                 arrows: true,
                 infinite: true,
-                autoplay: false
+                autoplay: true
             });
         });
     }
